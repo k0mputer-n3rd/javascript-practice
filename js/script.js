9a963afcdf4dfc3d292328306e6e629535db5a43
k0mputer n3rd <k0mputer-n3rd@outlook.com>
2022-02-27 15:41:03 -0800

    add extra stuff

--- js/script.js
+++ js/script.js
@@ -11,6 +11,13 @@ alert ("Check the Console for Messages"); // This shows that the JS has been lin
 console.log("This is Allen.");
 console.log(`I like Chinese Food.`)
 
+let year = 2022;
+let age = 67;
+let value = 1 / 0;
+
+console.log(isFinite(value));
+console.log(value);
+
 /***********\
 * style.css *
 \***********/
