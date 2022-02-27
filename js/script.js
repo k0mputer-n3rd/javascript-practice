a5b1ad474f275dc347b382563007fac9df220a16
k0mputer n3rd <k0mputer-n3rd@outlook.com>
2022-02-26 20:43:54 -0800

    Add comments and console messages.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-alert ("JavaScript Linked from HTML");
+/* BitWise JavaScript */
+/* Homework */
+/* K0mputer N3rd */
+/* 02/26/2022 */
+
+/***********\
+* script.js *
+\***********/
+
+alert ("Check the Console for Messages"); // This shows that the JS has been linked.
+console.log("This is Allen.");
+console.log(`I like Chinese Food.`)
+
+/***********\
+* style.css *
+\***********/
